fbab155dd381f25bb12912981ba619fd80987b40
Bruno Noriller <brunonoriller@gmail.com>
2021-08-05 10:37:16 -0300

    refactor: :recycle: updating optionsParse to new shapes

--- src/searchParser/optionsParse/optionsParse.spec.ts
+++ src/searchParser/optionsParse/optionsParse.spec.ts
@@ -3,25 +3,25 @@ import { optionsParse } from './optionsParse';
 describe('optionsParse', () => {
   describe('without a options keyword', () => {
     const searchWithoutOptions = 'search';
-    const emptyParsedOptions = { mode: 'OPTION', string: '' };
+    const emptyParsedOptions = null;
 
     it('should return itself and a empty options result', () => {
-      expect(optionsParse(searchWithoutOptions)).toEqual([
-        searchWithoutOptions,
-        emptyParsedOptions,
-      ]);
+      expect(optionsParse(searchWithoutOptions)).toEqual({
+        search: searchWithoutOptions,
+        parsedSearch: emptyParsedOptions,
+      });
     });
   });
 
   describe('without a proper option keyword', () => {
     const searchWithOptionAsValue = 'search with option that isnt a keyword';
-    const emptyParsedOptions = { mode: 'OPTION', string: '' };
+    const emptyParsedOptions = null;
 
     it('should not falsely use a word as a keyword', () => {
-      expect(optionsParse(searchWithOptionAsValue)).toEqual([
-        searchWithOptionAsValue,
-        emptyParsedOptions,
-      ]);
+      expect(optionsParse(searchWithOptionAsValue)).toEqual({
+        search: searchWithOptionAsValue,
+        parsedSearch: emptyParsedOptions,
+      });
     });
   });
 
@@ -29,38 +29,39 @@ describe('optionsParse', () => {
     it('should parse option (singular) keyword', () => {
       const searchWithOptionAsValue = 'search with option(option)';
       const searchWithOptionRemoved = 'search with';
-      const emptyParsedOptions = { mode: 'OPTION', string: 'option' };
+      const emptyParsedOptions = [{ mode: 'OPTION', payload: 'option' }];
 
-      expect(optionsParse(searchWithOptionAsValue)).toEqual([
-        searchWithOptionRemoved,
-        emptyParsedOptions,
-      ]);
+      expect(optionsParse(searchWithOptionAsValue)).toEqual({
+        search: searchWithOptionRemoved,
+        parsedSearch: emptyParsedOptions,
+      });
     });
 
     it('should parse options (plural) keyword', () => {
       const searchWithOptionAsValue = 'search with options(option)';
       const searchWithOptionRemoved = 'search with';
-      const emptyParsedOptions = { mode: 'OPTION', string: 'option' };
+      const emptyParsedOptions = [{ mode: 'OPTION', payload: 'option' }];
 
-      expect(optionsParse(searchWithOptionAsValue)).toEqual([
-        searchWithOptionRemoved,
-        emptyParsedOptions,
-      ]);
+      expect(optionsParse(searchWithOptionAsValue)).toEqual({
+        search: searchWithOptionRemoved,
+        parsedSearch: emptyParsedOptions,
+      });
     });
 
     it('should parse with multiple options keyword', () => {
       const searchWithOptionAsValue =
         'search with options(option1) option(option2 option3)';
       const searchWithOptionRemoved = 'search with';
-      const emptyParsedOptions = {
-        mode: 'OPTION',
-        string: 'option1 option2 option3',
-      };
+      const emptyParsedOptions = [
+        { mode: 'OPTION', payload: 'option1' },
+        { mode: 'OPTION', payload: 'option2' },
+        { mode: 'OPTION', payload: 'option3' },
+      ];
 
-      expect(optionsParse(searchWithOptionAsValue)).toEqual([
-        searchWithOptionRemoved,
-        emptyParsedOptions,
-      ]);
+      expect(optionsParse(searchWithOptionAsValue)).toEqual({
+        search: searchWithOptionRemoved,
+        parsedSearch: emptyParsedOptions,
+      });
     });
   });
 });
--- src/searchParser/optionsParse/optionsParse.ts
+++ src/searchParser/optionsParse/optionsParse.ts
@@ -1,23 +1,28 @@
 /* eslint-disable prettier/prettier */
-import { ParsedPart } from '../../shared/shapes';
+import { ParsedPart, ParsedResult } from '../../shared/shapes';
 import { cleanString } from '../../utils/cleanString';
 import { middleBetweenBracketsRegex } from '../../utils/regexes';
 
-export function optionsParse(search: string): [string, ParsedPart] {
+export function optionsParse(search: string): ParsedResult {
   const optionsPartRegex = /\b(options|option)\(.+?\)/gi;
 
-  const optionsPartFound = search.match(optionsPartRegex) || [];
+  const optionsPartFound = search.match(optionsPartRegex) || null;
 
-  const optionParsed =
-    optionsPartFound.length > 0
-      ? optionsPartFound.flatMap((option) => {
-        return option.match(middleBetweenBracketsRegex)[0].split(' ').filter(x => x);
-      })
-      : [];
+  if (optionsPartFound) {
+    const optionParsed = optionsPartFound.flatMap((option) => {
+      return option.match(middleBetweenBracketsRegex)[0].split(' ').filter(x => x);
+    });
 
-  const cleanedString = optionsPartFound.reduce((cleaned, option) => cleanString(cleaned, option), search);
-  return [
-    cleanedString,
-    { string: optionParsed.join(' '), mode: 'OPTION' }
-  ];
+    const cleanedString = optionsPartFound.reduce((cleaned, option) => cleanString(cleaned, option), search);
+
+    return {
+      search: cleanedString,
+      parsedSearch: optionParsed.map((payload): ParsedPart => ({ payload, mode: 'OPTION' }))
+    };
+  } else {
+    return {
+      search: search,
+      parsedSearch: null,
+    };
+  }
 }
