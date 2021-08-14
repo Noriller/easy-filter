52eb1a31734b40f3c92f9b38963cf5ef40c7a76f
Bruno Noriller <brunonoriller@gmail.com>
2021-08-14 13:18:52 -0300

    feat: :sparkles: adding the crawler for tags and chaining tags

---
+++ src/filterCore/objectCrawlers/tagCrawler.spec.ts
@@ -0,0 +1,114 @@
+import { tagCrawler } from './tagCrawler';
+
+describe('getTextCrawler', () => {
+  describe('with partial values', () => {
+    it('should return undefined without tags', () => {
+      const object = { some: 'object' };
+      const tags = null;
+      expect(tagCrawler(object, tags)).toBe(undefined);
+    });
+    it('should return undefined without object', () => {
+      const tags = 'first.second';
+      expect(tagCrawler(undefined, tags)).toBe(undefined);
+    });
+  });
+
+  describe('with existing tags', () => {
+    const value = 'value';
+    const object = {
+      first: {
+        second: value,
+      },
+    };
+    it('should return the value from the tag', () => {
+      const tags = 'first';
+      expect(tagCrawler(object, tags)).toBe(object.first);
+    });
+    it('should return the value from the chaining tag', () => {
+      const tags = 'first.second';
+      expect(tagCrawler(object, tags)).toBe(value);
+    });
+  });
+
+  describe('without existing tags', () => {
+    const value = 'value';
+    const object = {
+      first: {
+        second: value,
+      },
+    };
+    it('should return undefined when the first tag dont exist', () => {
+      const tags = 'nullTag';
+      expect(tagCrawler(object, tags)).toBe(undefined);
+    });
+    it('should return undefined if the first tag from the chaining tag dont exist', () => {
+      const tags = 'nullTag.second';
+      expect(tagCrawler(object, tags)).toBe(undefined);
+    });
+  });
+
+  describe('with arrays', () => {
+    it('should return the index of the array', () => {
+      const object = [1, 2, 3, 4];
+      const tags = '0';
+      expect(tagCrawler(object, tags)).toBe(1);
+    });
+    it('should return the value from the subobject', () => {
+      const object = [{ value: 1 }, { value: 2 }];
+      const tags = '0.value';
+      expect(tagCrawler(object, tags)).toBe(1);
+    });
+  });
+
+  describe('with complext objects and arrays', () => {
+    const complexObject = {
+      firstTag: 'just a string',
+      secondTag: [
+        { value: 1 },
+        {
+          subObject: {
+            value: 2,
+          },
+        },
+        {
+          subObject: {
+            yetAnotherSubObject: {
+              value: 2,
+            },
+          },
+        },
+      ],
+      thirdTag: {
+        1: { value: 'string 1' },
+        2: ['a', 'b', 'c'],
+      },
+    };
+
+    it('should return the entire subobject', () => {
+      const tags = 'secondTag';
+      expect(tagCrawler(complexObject, tags)).toBe(complexObject.secondTag);
+    });
+
+    it('should return the subobject inside the array', () => {
+      const tags = 'secondTag.2';
+      expect(tagCrawler(complexObject, tags)).toBe(complexObject.secondTag[2]);
+    });
+
+    it('should return the subobject inside the object in the array', () => {
+      const tags = 'secondTag.2.subObject';
+      expect(tagCrawler(complexObject, tags)).toBe(
+        complexObject.secondTag[2].subObject,
+      );
+    });
+
+    it('should not find a object outside of the bounds of the array inside the complex object', () => {
+      const tags = 'secondTag.999.subObject';
+      expect(tagCrawler(complexObject, tags)).toBe(undefined);
+    });
+
+    it('should find a tag that is a number and not an array index', () => {
+      const tags = 'thirdTag.1';
+      expect(tagCrawler(complexObject, tags)).toBe(complexObject.thirdTag[1]);
+    });
+  });
+});
---
+++ src/filterCore/objectCrawlers/tagCrawler.ts
@@ -0,0 +1,13 @@
+function tagCrawlerWrapper(object: unknown, tag: string) {
+  if (!tag || !object) return;
+  const tags = tag.split('.');
+  return tagCrawlerRecursion(object, tags);
+}
+
+function tagCrawlerRecursion(object: unknown, tags: string[]) {
+  if (!tags || !object || tags.length === 0) return object;
+  const [firstTag, ...restTags] = tags;
+  return tagCrawlerRecursion(object[firstTag], restTags);
+}
+
+export { tagCrawlerWrapper as tagCrawler };
