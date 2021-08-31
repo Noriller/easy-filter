094d53c2cd8fa54ae47a4dc07121354b6caaf32a
Bruno Noriller <brunonoriller@gmail.com>
2021-08-31 19:42:02 -0300

    docs: :memo: removeDiacritics documentation

--- src/utils/removeDiacritics.ts
+++ src/utils/removeDiacritics.ts
@@ -1,3 +1,8 @@
+/**
+ * Takes a string and removes all diacritics.
+ * @param string
+ * @returns the string without diacritics: "Crème brûlée" returns as "Creme brulee"
+ */
 export function removeDiacritics(string: string): string {
   return string.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
