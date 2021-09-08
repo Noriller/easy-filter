ed6e46266fe00979164170a319ad9c3a83c8b5ca
Bruno Noriller <brunonoriller@gmail.com>
2021-09-08 16:27:46 -0300

    docs: :memo: adding annotations to objectCrawlers

--- src/filterCore/objectCrawlers/getTextCrawler.ts
+++ src/filterCore/objectCrawlers/getTextCrawler.ts
@@ -1,4 +1,11 @@
+/**
+ * Traverse objects (or primitives) recursively and return all values concatenaded together, separated by space.
+ *
+ * @param object basically anything
+ * @returns a concatenation of all values of the given object recursively.
+ */
 export function getTextCrawler(object: unknown): string {
+  // This basically returns the string representation of primitive values, used as exit condition to the recursion
   if (object !== Object(object)) return object?.toString();
 
   return Object.values(object).flatMap(getTextCrawler).join(' ');
